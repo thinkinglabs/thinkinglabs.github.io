829cb4871df1033488f4c6e71e2861919d0a2ea8
Thierry de Pauw <thierry@thinkinglabs.io>
2020-06-28 07:09:27 +0200

    Introduce the terraform dns zone module

---
+++ dns/modules/zone/main.tf
@@ -0,0 +1,79 @@
+
+locals {
+  has_google_site_verification = var.google_site_verification == null ? 0 : 1
+
+  has_spf  = var.spf == null ? 0 : 1
+  has_dkim = var.dkim == null ? 0 : 1
+
+  # OVH does not accept a TTL lower than 60 !
+  ttl_ns  = 86400
+  ttl_mx  = 28800
+  ttl_a   = 10800
+  ttl_spf = 3600
+}
+
+resource "ovh_domain_zone_record" "ns" {
+  count     = length(var.name_servers)
+  zone      = var.zone
+  fieldtype = "NS"
+  ttl       = local.ttl_ns
+  target    = var.name_servers[count.index]
+}
+
+resource "ovh_domain_zone_record" "a" {
+  count     = length(var.ipv4)
+  zone      = var.zone
+  fieldtype = "A"
+  ttl       = local.ttl_a
+  target    = var.ipv4[count.index]
+}
+
+resource "ovh_domain_zone_record" "cname" {
+  count     = length(var.aliases)
+  zone      = var.zone
+  subdomain = var.aliases[count.index].subdomain
+  fieldtype = "CNAME"
+  ttl       = local.ttl_a
+  target    = var.aliases[count.index].target
+}
+
+resource "ovh_domain_zone_redirection" "this" {
+  count     = length(var.redirections)
+  zone      = var.zone
+  subdomain = var.redirections[count.index].subdomain
+  type      = var.redirections[count.index].type
+  target    = var.redirections[count.index].target
+}
+
+resource "ovh_domain_zone_record" "google_site_verification" {
+  count     = local.has_google_site_verification
+  zone      = var.zone
+  fieldtype = "TXT"
+  ttl       = local.ttl_a
+  target    = "\"google-site-verification=${var.google_site_verification}\""
+}
+
+resource "ovh_domain_zone_record" "mx" {
+  count     = length(var.mx)
+  zone      = var.zone
+  fieldtype = "MX"
+  ttl       = local.ttl_mx
+  target    = var.mx[count.index]
+}
+
+resource "ovh_domain_zone_record" "spf" {
+  count     = local.has_spf
+  zone      = var.zone
+  fieldtype = "TXT"
+  ttl       = local.ttl_spf
+  target    = var.spf
+}
+
+resource "ovh_domain_zone_record" "dkim" {
+  count     = local.has_dkim
+  zone      = var.zone
+  subdomain = "google._domainkey"
+  fieldtype = "TXT"
+  ttl       = local.ttl_spf
+  target    = var.dkim
+}
---
+++ dns/modules/zone/variables.tf
@@ -0,0 +1,58 @@
+
+variable "zone" {
+  description = "DNS domain"
+  type        = string
+}
+
+variable "name_servers" {
+  description = "IP or domain name of name servers"
+  type        = list(string)
+}
+
+variable "ipv4" {
+  description = "List of A record target IPs"
+  type        = list(string)
+}
+
+variable "aliases" {
+  description = "List of CNAME aliases"
+  type        = list(object({
+    subdomain = string
+    target    = string
+  }))
+  default     = []
+}
+
+variable "redirections" {
+  description = "List of HTTP redirections"
+  type        = list(object({
+    subdomain = string
+    type      = string
+    target    = string
+  }))
+  default     = []
+}
+
+variable "google_site_verification" {
+  description = "Verification code used by GSuite to check for ownership"
+  type        = string
+  default     = null
+}
+
+variable "mx" {
+  description = "MX records"
+  type        = list(string)
+  default     = []
+}
+
+variable "spf" {
+  description = "SPF record"
+  type        = string
+  default     = null
+}
+
+variable "dkim" {
+  description = "DKIM record"
+  type        = string
+  default     = null
+}
