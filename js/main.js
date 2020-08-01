95bfe1c8dddedbe88065a6e1befcc0127dddaba5
Thierry de Pauw <thierry@thinkinglabs.io>
2020-08-02 00:20:10 +0200

    Remove unused google maps mouse behaviour manipulation
    See #4

--- js/main.js
+++ js/main.js
@@ -1,22 +1,5 @@
 
 $(document).ready(function () {
-    /*==============================================================*/
-    //google map - mouse scrolling wheel behavior - START CODE
-    /*==============================================================*/
-    // you want to enable the pointer events only on click;
-
-    $('#map_canvas1').addClass('scrolloff'); // set the pointer events to none on doc ready
-    $('#canvas1').on('click', function () {
-        $('#map_canvas1').removeClass('scrolloff'); // set the pointer events true on click
-    });
-    // you want to disable pointer events when the mouse leave the canvas area;
-
-    $("#map_canvas1").mouseleave(function () {
-        $('#map_canvas1').addClass('scrolloff'); // set the pointer events to none when mouse leaves the map area
-    });
-    /*==============================================================*/
-    //google map - mouse scrolling wheel behavior - END CODE
-    /*==============================================================*/
 
     /*==============================================================*/
     //Search - START CODE
