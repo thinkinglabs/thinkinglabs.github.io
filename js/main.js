a50c4faacf2f1449a604613e135c08508b6177df
Thierry de Pauw <thierry@thinkinglabs.io>
2020-08-02 23:55:23 +0200

    Remove unused final countdown jQuery plugin
    See #4

--- js/main.js
+++ js/main.js
@@ -103,21 +103,6 @@ $(window).resize(function () {
 /*==============================================================*/
 
 /*==============================================================*/
-//Countdown Timer - START CODE
-/*==============================================================*/
-//TODO remove final countdown counter.js
-$('#counter-underconstruction').countdown('2015/12/15 12:00:00').on('update.countdown', function (event) {
-    var $this = $(this).html(event.strftime('' + '<div class="counter-container"><div class="counter-box first"><div class="number">%-D</div><span>Day%!d</span></div>' + '<div class="counter-box"><div class="number">%H</div><span>Hours</span></div>' + '<div class="counter-box"><div class="number">%M</div><span>Minutes</span></div>' + '<div class="counter-box last"><div class="number">%S</div><span>Seconds</span></div></div>'))
-});
-$('#counter-wedding').countdown('2015/09/20 12:00:00').on('update.countdown', function (event) {
-    var $this = $(this).html(event.strftime('' + '<div class="counter-container"><div class="counter-box first"><div class="number">%-D</div><span>Day%!d</span></div>' + '<div class="counter-box"><div class="number">%H</div><span>Hours</span></div>' + '<div class="counter-box"><div class="number">%M</div><span>Minutes</span></div>' + '<div class="counter-box last"><div class="number">%S</div><span>Seconds</span></div></div>'))
-});
-/*==============================================================*/
-//Countdown Timer - END CODE
-/*==============================================================*/
-
-
-/*==============================================================*/
 //Scroll To Top - START CODE
 /*==============================================================*/
 $(window).scroll(function () {
