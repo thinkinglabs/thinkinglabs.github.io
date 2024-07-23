792e0c0ef328c225e5002ebb1f7b2373415cb18c
Thierry de Pauw <thierry@thinkinglabs.io>
2024-07-23 15:42:45 +0200

    Move ovh provider version declaration to required_providers

--- dns/main.tf
+++ dns/main.tf
@@ -1,7 +1,6 @@
 
 provider "ovh" {
   endpoint           = "ovh-eu"
-  version            = "~> 0.5"
   application_key    = var.ovh_application_key
   application_secret = var.ovh_application_secret
   consumer_key       = var.ovh_consumer_key
